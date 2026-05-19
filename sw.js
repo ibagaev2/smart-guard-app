7c18e73994357397bf3776934b4ed303f5db196b
ibagaev2 <bagaeviman@icloud.com>
2026-05-19 15:48:08 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'ALARM') {
+        self.registration.showNotification('🚨 Smart-Guard ALARM!', {
+            body: 'Dein Rucksack wurde bewegt!',
+            icon: '/icon.png',
+            badge: '/icon.png',
+            vibrate: [500, 200, 500, 200, 500],
+            requireInteraction: true,
+            tag: 'smart-guard-alarm'
+        });
+    }
+});
